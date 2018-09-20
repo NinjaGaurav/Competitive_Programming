ac5874e381f07052337971a2d6cd92afef91782c
Gaurav Bholla <37663361+NinjaGaurav@users.noreply.github.com>
2018-09-20 19:28:11 +0530

    Create Very Big Sum.cpp

---
+++ Very Big Sum.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long n,s=0,ar;
+    cin>>n;
+    while(n--)
+    {   
+        cin>>ar;
+        s+=ar;
+    }
+    cout<<s;
+    return 0;
+}
