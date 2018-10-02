f4a74efb0eafe02d537d53bb6189a94b04b3c05e
Gaurav Bholla <37663361+NinjaGaurav@users.noreply.github.com>
2018-10-02 17:06:33 +0530

    Create Lisa's Workbook.cpp
    https://www.hackerrank.com/challenges/lisa-workbook/problem

---
+++ Lisa's Workbook.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the workbook function below.
+int workbook(int n, int k, vector<int> arr) {
+    int tk=0,pg=1,ans=0;
+for(int i=0;i<n;i++)
+{   tk=1;
+    for(int j=1;j<=arr[i];j++,tk++)
+    {    
+        if(tk==k+1)
+        {pg++;tk=1;}
+        if(j==pg)
+            ans++;
+
+        
+    }
+        pg++;
+}
+return ans;
+}
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nk_temp;
+    getline(cin, nk_temp);
+
+    vector<string> nk = split_string(nk_temp);
+
+    int n = stoi(nk[0]);
+
+    int k = stoi(nk[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = workbook(n, k, arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
