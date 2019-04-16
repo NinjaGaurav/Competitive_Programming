e51f186ef8b1d8eee011f5e743854518ab82d81c
Gaurav Bholla <37663361+NinjaGaurav@users.noreply.github.com>
2019-04-16 19:23:21 +0530

    Update superlarge_numbers.py

--- superlarge_numbers.py
+++ superlarge_numbers.py
@@ -2,7 +2,7 @@ a = int(input("ENTER FIRST NUMBER: "))
 b = int(input("ENTER SECOND NUMBER: "))
 c = int(input("ENTER THIRD NUMBER: "))
 result = a
-if b > result & b > c:
+if b > result and b > c:
     result = b
 elif c > result:
     result = c
