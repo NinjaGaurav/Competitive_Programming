54687b27bd676bc52b519821dd39399c9f0177b5
Gaurav Bholla <37663361+NinjaGaurav@users.noreply.github.com>
2020-01-08 20:47:37 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define repi(i, a, b) for(int i = a; i >= (b); --i)
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#define rep(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
 #define trav(a, x) for(auto& a : x)
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
@@ -16,6 +16,7 @@ using namespace std;
 #define PB push_back
 #define MP make_pair
 #define MOD 1000000007
+#define INF 1152921504606846976LL
 typedef long long ll;
 typedef pair<ll, ll> pii;
 typedef vector<ll> vi;
