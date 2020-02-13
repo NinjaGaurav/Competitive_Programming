4a89a94f90dd6f3c3b5df2abf7a51a90a0a53ab8
Gaurav Bholla <37663361+NinjaGaurav@users.noreply.github.com>
2020-02-13 10:44:13 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,36 @@
 using namespace std;
 
 #define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) { cerr << *it << " = " << a << endl; err(++it, args...); }
+template <typename T1, typename T2>
+inline std::ostream& operator << (std::ostream& os, const std::pair<T1, T2>& p) { return os << "(" << p.first << ": " << p.second << ")";}
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const std::vector<T>& v) { bool first = true; for(unsigned int i = 0; i < v.size(); i++) {
+        if(!first) os << " "; os << v[i]; first = false; }
+       return os; }
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const std::set<T>& v) { bool first = true; os << "{";
+    for (typename std::set<T>::const_iterator ii = v.begin(); ii != v.end(); ++ii) {
+        if(!first) os << ", "; os << *ii; first = false; }
+    return os << "}"; }
+template<typename T1, typename T2>
+inline std::ostream &operator << (std::ostream & os,const std::map<T1, T2>& v) {
+    bool first = true; os << "{";
+    for (typename std::map<T1, T2>::const_iterator ii = v.begin(); ii != v.end(); ++ii) {
+        if(!first) os << ", "; os << *ii ; first = false; }
+    return os << "}"; }
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const std::unordered_set<T>& v) {
+    return os << std::set<T>(v.begin(), v.end()); }
+template<typename T1, typename T2>
+inline std::ostream &operator << (std::ostream & os,const std::unordered_map<T1, T2>& v) {
+    return os << std::map<T1, T2>(v.begin(), v.end()); }
 #define rep(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
 #define trav(a, x) for(auto& a : x)
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
-#define endl '\n'
 #define FI first
 #define SE second
 #define PB push_back
@@ -24,8 +49,10 @@ typedef vector< vector<ll> > matrix;
 
 int main() {
 
-	cin.sync_with_stdio(0); cin.tie(0);
-	cin.exceptions(cin.failbit);
-	
-	return 0;
+    cin.sync_with_stdio(0); cin.tie(0);
+    cin.exceptions(cin.failbit);
+
+
+
+    return 0;
 }
